a711a61a524cb2883516dc6333f2165795abfd77
mylesgrxnt <grantmx@bc.edu>
2025-01-20 16:26:52 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Myles Grant on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
